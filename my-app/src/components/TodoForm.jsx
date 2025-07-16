1cdb484dbc265e8e9c1b30b3a06f40d5ec054530
Valfran <valfran-goncalves@hotmail.com>
2025-07-16 07:40:08 -0300

    refactor: use Recoil to add tasks and clear input

--- my-app/src/components/TodoForm.jsx
+++ my-app/src/components/TodoForm.jsx
@@ -1,31 +1,41 @@
 // Campo de Input e botao de adicionar
-import { useState } from 'react';
-import { useContext } from 'react';
-import TodoContext from '../context/TodoContext';
+import { todoListState } from "../atoms/todoListAtom";
+import { useState } from "react";
+import { useSetRecoilState } from "recoil";
+
 
 function TodoForm() {
-  const [input, setInput] = useState('');
-  const { adicionarTarefa } = useContext(TodoContext);
+  const [input, setInput] = useState("");
+  const setTarefas = useSetRecoilState(todoListState);
+  const adicionarTarefa = (texto) =>{
+    const novaTarefa = {
+      id: Date.now(),
+      texto,
+      concluida: false,
+    };
+            setTarefas((prev) => [...prev, novaTarefa]);
+  };
 
   const adicionar = (e) => {
     e.preventDefault();
-    if (input.trim() === '') return;
     adicionarTarefa(input);
-    setInput('');
+    setInput("");
   };
 
-
   return (
-    <form className="input-group mb-3">
-      <label className='input-group-text' htmlFor="novaTarefa">Nova tarefa: </label>
-      <input className='form-control'
+    <form className="input-group mb-3" onSubmit={adicionar}>
+      <label className="input-group-text" htmlFor="novaTarefa">
+        Nova tarefa:{" "}
+      </label>
+      <input
+        className="form-control"
         type="text"
         placeholder="Digite sua tarefa"
         id="novaTarefa"
         value={input}
         onChange={(e) => setInput(e.target.value)}
       />
-      <button className='btn btn-primary' onClick={adicionar}>
+      <button className="btn btn-primary" type="submit">
         Adicionar
       </button>
     </form>
