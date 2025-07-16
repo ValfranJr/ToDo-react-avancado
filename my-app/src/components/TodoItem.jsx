4b763e06216f10d34c7a3399110098e576286b3c
Valfran <valfran-goncalves@hotmail.com>
2025-07-16 07:40:54 -0300

    fix: fix prop destructuring and connect actions to Recoil

--- my-app/src/components/TodoItem.jsx
+++ my-app/src/components/TodoItem.jsx
@@ -1,16 +1,51 @@
 //Todo Item cada tarefa com opçao de concluir e remover
-import React from 'react';
-const TodoItem = React.memo(function TodoItem({ id, texto, tarefaConcluida, marcarConcluida, removerTarefa }) {
+import React from "react";
+import { useSetRecoilState } from "recoil";
+import { todoListState } from "../atoms/todoListAtom";
+
+const TodoItem = React.memo(function TodoItem({id, texto, tarefaConcluida}) {
+  const setTarefas = useSetRecoilState(todoListState);
+
+  const marcarConcluida = (id) => {
+    setTarefas((prevTarefas) =>
+      prevTarefas.map((tarefa) =>
+        tarefa.id === id ? { ...tarefa, concluida: !tarefa.concluida } : tarefa
+      )
+    );
+  };
+
+  const removerTarefa = (id) => {
+    setTarefas((prevTarefas) =>
+      prevTarefas.filter((tarefa) => tarefa.id !== id)
+    );
+  };
+
   return (
     <li className="list-group-item d-flex justify-content-between align-items-center">
-      <span style={{ textDecoration: tarefaConcluida ? 'line-through' : 'none' }}>
+      <span
+        style={{ textDecoration: tarefaConcluida ? "line-through" : "none" }}
+      >
         {texto}
       </span>
-      {/* Checkbox para marcar tarefa como concluida */}
-      <label htmlFor="concluido" className="form-check-label">Concluir ➡</label>
-      <input className='form-check-input' type="checkbox" name="concluido" onChange={() => marcarConcluida(id)} checked={tarefaConcluida}/>
 
-      <button className="btn btn-sm btn-danger" onClick={() => removerTarefa(id)}>Remover</button>
+      {/* Checkbox para marcar tarefa como concluida */}
+      <label htmlFor="concluido" className="form-check-label">
+        Concluir ➡
+      </label>
+      <input
+        className="form-check-input"
+        type="checkbox"
+        name="concluido"
+        onChange={() => marcarConcluida(id)}
+        checked={tarefaConcluida}
+      />
+
+      <button
+        className="btn btn-sm btn-danger"
+        onClick={() => removerTarefa(id)}
+      >
+        Remover
+      </button>
     </li>
   );
 });
